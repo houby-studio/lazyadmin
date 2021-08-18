7932915e6add5c2d9d06dd3faf03d0c0b7507b9f
Jakub Šindelář <megastary@houby-studio.eu>
2021-08-18 22:44:15 +0200

    Configured custom user data folder
    Also removed web resources from project solution.

--- HoubyStudio.LazyAdmin.DesktopApp/HoubyStudio.LazyAdmin.DesktopApp/MainWindow.xaml.cs
+++ HoubyStudio.LazyAdmin.DesktopApp/HoubyStudio.LazyAdmin.DesktopApp/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.IO;
 using System.Windows;
 
 namespace HoubyStudio.LazyAdmin.DesktopApp
@@ -8,18 +10,53 @@ namespace HoubyStudio.LazyAdmin.DesktopApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Uri index;
+        /// <summary>
+        /// Resolves to the full path of the user data folder for WebView2 component.
+        /// </summary>
+        /// <returns> Usually the returned folder path will conform to this format:
+        /// <c>C:\Users\{username}\AppData\Local\LazyAdmin</c></returns>
+        private readonly string _cacheFolderPath;
 
-        public string LazyAdminUserDataFolder
-        {
-            get { return (string)System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "TestEdge"); }
-        }
+        /// <summary>
+        /// Resolves to the index.html file, which gets rendered by WebView2 component.
+        /// </summary>
+        /// <returns> Usually the returned file path will conform to this format:
+        /// <c>C:\Users\{username}\AppData\Local\LazyAdmin\EBWebView\WebResources\index.html</c></returns>
+        private readonly Uri _indexFilePath;
 
         public MainWindow()
         {
             InitializeComponent();
-            index = new Uri(System.IO.Path.Combine(Environment.CurrentDirectory, "web/spa/index.html"));
-            webView.Source = index;
+
+            _cacheFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "LazyAdmin");
+            _indexFilePath = new Uri(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "LazyAdmin/EBWebView/WebResources/index.html"));
+        }
+
+        protected override async void OnContentRendered(EventArgs e)
+        {
+            base.OnContentRendered(e);
+
+            try
+            {
+                // Create new environment configuration pointing to correct User Data Folder
+                CoreWebView2Environment webView2Environment = await CoreWebView2Environment.CreateAsync(null, _cacheFolderPath);
+                await webView.EnsureCoreWebView2Async(webView2Environment);
+
+                // TODO: Check if _indexFilePath file exists and if not, try to download it from Github.
+                webView.Source = new UriBuilder(_indexFilePath).Uri;
+            }
+            catch (Exception)
+            {
+                // TODO: String to resource for translation
+                Shutdown("An error occurred when starting the browser. Browser window will close.", "Error Occurred");
+            }
+        }
+
+        // TODO: String to resource for translation
+        private void Shutdown(string message, string caption = "Information")
+        {
+            MessageBox.Show(this, message, caption);
+            Application.Current.Shutdown();
         }
 
     }
