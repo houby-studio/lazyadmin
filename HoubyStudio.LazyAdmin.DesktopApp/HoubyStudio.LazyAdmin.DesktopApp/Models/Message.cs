f65dcd7af89faa2ca48fae8a21f5e2b73d101973
Jakub Šindelář <megastary@houby-studio.eu>
2021-08-26 07:31:18 +0200

    WIP code reorganization #8

---
+++ HoubyStudio.LazyAdmin.DesktopApp/HoubyStudio.LazyAdmin.DesktopApp/Models/Message.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoubyStudio.LazyAdmin.DesktopApp.Models
+{
+    class Message
+    {
+        private Guid _token;
+
+        public Guid Token
+        {
+            set => _token = value;
+            get => _token;
+        }
+
+        public Message()
+        {
+            // PowerShell does not send token to javascript
+        }
+
+        public Message(Guid token)
+        {
+            // Javascript sends token to validate source
+            Token = token;
+        }
+    }
+}
